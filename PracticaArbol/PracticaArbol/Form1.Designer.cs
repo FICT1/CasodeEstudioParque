973cda488de4bd99025dafce597bdfc3bdb259df
FICT1 <ficallejas@uamv.edu.ni>
2025-11-14 08:24:21 -0600

    Primer commit

---
+++ PracticaArbol/PracticaArbol/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace PracticaArbol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tvParque = new System.Windows.Forms.TreeView();
+            this.lblLugar = new System.Windows.Forms.Label();
+            this.tbParque = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnContar = new System.Windows.Forms.Button();
+            this.btnNivel = new System.Windows.Forms.Button();
+            this.btnBusqueda = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tvParque
+            // 
+            this.tvParque.Location = new System.Drawing.Point(30, 116);
+            this.tvParque.Name = "tvParque";
+            this.tvParque.Size = new System.Drawing.Size(280, 264);
+            this.tvParque.TabIndex = 0;
+            // 
+            // lblLugar
+            // 
+            this.lblLugar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLugar.Location = new System.Drawing.Point(50, 32);
+            this.lblLugar.Name = "lblLugar";
+            this.lblLugar.Size = new System.Drawing.Size(121, 39);
+            this.lblLugar.TabIndex = 1;
+            this.lblLugar.Text = "Lugar: ";
+            // 
+            // tbParque
+            // 
+            this.tbParque.Location = new System.Drawing.Point(39, 74);
+            this.tbParque.Name = "tbParque";
+            this.tbParque.Size = new System.Drawing.Size(172, 22);
+            this.tbParque.TabIndex = 2;
+            this.tbParque.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(237, 12);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(102, 43);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(237, 67);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(102, 43);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnContar
+            // 
+            this.btnContar.Location = new System.Drawing.Point(12, 400);
+            this.btnContar.Name = "btnContar";
+            this.btnContar.Size = new System.Drawing.Size(102, 43);
+            this.btnContar.TabIndex = 5;
+            this.btnContar.Text = "Contar";
+            this.btnContar.UseVisualStyleBackColor = true;
+            this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
+            // 
+            // btnNivel
+            // 
+            this.btnNivel.Location = new System.Drawing.Point(131, 400);
+            this.btnNivel.Name = "btnNivel";
+            this.btnNivel.Size = new System.Drawing.Size(102, 43);
+            this.btnNivel.TabIndex = 6;
+            this.btnNivel.Text = "Nivel";
+            this.btnNivel.UseVisualStyleBackColor = true;
+            this.btnNivel.Click += new System.EventHandler(this.btnNivel_Click);
+            // 
+            // btnBusqueda
+            // 
+            this.btnBusqueda.Location = new System.Drawing.Point(252, 400);
+            this.btnBusqueda.Name = "btnBusqueda";
+            this.btnBusqueda.Size = new System.Drawing.Size(102, 43);
+            this.btnBusqueda.TabIndex = 7;
+            this.btnBusqueda.Text = "Buscar";
+            this.btnBusqueda.UseVisualStyleBackColor = true;
+            this.btnBusqueda.Click += new System.EventHandler(this.btnBusqueda_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(382, 450);
+            this.Controls.Add(this.btnBusqueda);
+            this.Controls.Add(this.btnNivel);
+            this.Controls.Add(this.btnContar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tbParque);
+            this.Controls.Add(this.lblLugar);
+            this.Controls.Add(this.tvParque);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvParque;
+        private System.Windows.Forms.Label lblLugar;
+        private System.Windows.Forms.TextBox tbParque;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnContar;
+        private System.Windows.Forms.Button btnNivel;
+        private System.Windows.Forms.Button btnBusqueda;
+    }
+}
+
---
+++ PracticaArbol/PracticaArbol/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaArbol
+{
+    public partial class Form1 : Form
+    {
+        TreeNode nodo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (tvParque.Nodes.Count == 0)
+            {
+                TreeNode root = new TreeNode("Presidente General (CEO)");
+                root.Name = "CEO";
+                tvParque.Nodes.Add(root);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (tbParque.Text == "")
+                return;
+
+            TreeNode nuevo = new TreeNode(tbParque.Text);
+
+            if (tvParque.SelectedNode != null)
+            {
+                tvParque.SelectedNode.Nodes.Add(nuevo);
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un nodo para agregar un elemento.");
+                return;
+            }
+
+            tbParque.Clear();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (tvParque.SelectedNode != null)
+            {
+                if (tvParque.SelectedNode.Name == "CEO")
+                {
+                    MessageBox.Show("El nodo 'Presidente General (CEO)' no puede eliminarse.");
+                    return;
+                }
+
+                tvParque.SelectedNode.Remove();
+                tbParque.Clear();
+            }
+        }
+
+        private void btnBusqueda_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNivel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnContar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
